d47ea3b7a69ae944c23ab7102dbf9a2e54415cbf
Dani <daniele.ddsp@gmail.com>
2022-04-18 01:06:56 -0300

    Create Turma.java

---
+++ Ex02/Turma.java
@@ -0,0 +1,11 @@
+package poo.lista3.Ex2;
+
+public class Turma {
+    
+    int quantidadeAlunos;
+    float mediaAluno;
+    float mediaTurma;
+      
+    public void calculaMediaTurma(){}
+    
+}
