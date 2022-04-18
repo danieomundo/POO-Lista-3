51c36eede4c4a490faf1b9cdbf75b6b4c7408ece
Dani <daniele.ddsp@gmail.com>
2022-04-18 01:05:10 -0300

    Create Aluno.java

---
+++ Ex02/Aluno.java
@@ -0,0 +1,14 @@
+package poo.lista3.Ex2;
+
+public class Aluno {
+    
+    String nome;
+    float notas;
+      
+    public void calculaCoeficienteRendimento(){}
+    
+    public void calculaMediaAluno(){}
+    
+    public void exibirHistorico(){}
+    
+}
