d0ed6bfc735183dec8327a09c368334d9c89f810
Dani <daniele.ddsp@gmail.com>
2022-04-18 00:44:07 -0300

    Create Carro.java

---
+++ Ex01/Carro.java
@@ -0,0 +1,33 @@
+package poo.lista3.Ex1;
+
+/*
+
+Enunciado:
+
+Para cada uma das situações a seguir, implemente a classe Carro. 
+
+Obs: é suficiente colocar a assinatura dos métodos (eles não precisam ser implementados) 
+
+- Sistema de uma locadora de carros
+- Sistema de uma revendedora de carros
+- Sistema de uma oficina mecânica
+- Sistema do DETRAN
+ 
+*/
+
+public class Carro {
+    
+    private String cor;
+    private String marca;
+    private String modelo;
+    private String placa;
+
+    public void locadora(Carro carro){}
+    
+    public void revendedora(Carro carro){}
+    
+    public void oficina(Carro carro){}
+    
+    public void detran(Carro carro){}
+      
+}
