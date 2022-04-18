33589adcb2f6d327cab89a3f9f9086a9512ab946
Dani <daniele.ddsp@gmail.com>
2022-04-18 01:05:45 -0300

    Create Disciplina.java

---
+++ Ex02/Disciplina.java
@@ -0,0 +1,13 @@
+package poo.lista3.Ex2;
+
+public class Disciplina {
+    
+    String nome;
+    String ementa;
+    String horario;
+    String departamento;
+    String cargaHoraria;
+    
+    public void HorarioAula(){}         
+                   
+}
