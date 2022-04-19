a1c2b391b10d163dfc3889fc4ca92d5b12f88147
Dani <daniele.ddsp@gmail.com>
2022-04-18 23:09:59 -0300

    Create Main.java

---
+++ Ex04/Main.java
@@ -0,0 +1,17 @@
+/*
+
+Enunciado: 
+
+Crie uma classe Data com as seguintes capacidades:
+
+Gerar saída da data em múltiplos formatos, como: DD/MM/AAAA; Agosto 08, 2017; DDD AAAA;
+Utilize construtores sobrecarregados para criar objetos Data inicializados com datas no formato definido em cima.
+
+- No primeiro caso, o construtor deve receber 3 valores inteiros.
+- No segundo caso, deve receber uma String e 2 valores inteiros.
+- No terceiro caso deve receber 2 valores inteiros, o primeiro representando o número de dias no ano.
+
+Faça uma classe principal que implemente o método main que deverá instanciar 2 objetos de cada formato e depois imprimir cada um deles.
+
+*/
+
